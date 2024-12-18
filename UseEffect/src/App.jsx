7cbbc4010424a10ce713b7b082a3761f0dc91fcd
divyeshdankhara34@gmail.com <divyeshdankhara34@gmail.com>
2024-12-18 16:36:31 +0530

    18-12-2024

---
+++ UseEffect/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Effect from './components/Effect'
+// import Task from './components/Task'
+
+function App() {
+
+  return (
+    <Effect/>
+    // <Task/>
+  )
+
+}
+
+export default App
